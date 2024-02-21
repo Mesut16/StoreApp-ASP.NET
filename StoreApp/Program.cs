ca956c30c5be683c6f3f63b811e7cda3685ca216
mesutyilaiz166@gmail.com <mesutyilaiz16@gmail.com>
2024-02-21 18:28:51 +0300

    proje

---
+++ StoreApp/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Contracts;
+using Services;
+using Services.Contracts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<RepositoryContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"), 
+    b => b.MigrationsAssembly("StoreApp"));
+});
+
+builder.Services.AddScoped<IRepositoryManager,RepositoryManger>();
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
+
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
+builder.Services.AddScoped<IProductService, ProductManager>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
